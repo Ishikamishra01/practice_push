fb9826b51f03c38d80e1c05ea7b1d08cff4b4b9d
Ishika Mishra <160226117+Ishikamishra01@users.noreply.github.com>
2026-08-22 06:12:32 +0530

    added here

--- demo.py
+++ demo.py
@@ -1,8 +1,11 @@
-def my_function():
+"""Simple arithmetic demonstration."""
 
+
+def my_function():
+    """Perform a simple arithmetic calculation."""
     a = 4
     b = 2
-    return a + b - a/b
+    return a + b - a / b
 
 
 my_function()
